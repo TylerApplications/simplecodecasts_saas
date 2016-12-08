4cbdfa1fb21498ec527e2e128e9d1ca524ef1fd3
Tyler Reynolds <tylermalone14@gmail.com>
2016-12-08 01:44:14 +0000

    added redirect before actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
     def new
         #form where user can fill out own profile
         @user = User.find( params[:user_id] )
@@ -19,6 +22,25 @@ class ProfilesController < ApplicationController
         @user = User.find( params[:user_id] )
         @profile = @user.profile
     end
+    
+    def update
+         @user = User.find( params[:user_id] )
+         @profile = @user.profile
+         if @profile.update_attributes(profile_params)
+             flash[:success] = "Profile has been updated"
+             redirect_to user_path( params[:user_id] )
+         else
+             render action: :edit
+         end
+    end
+    
+    def only_current_user
+        @user = User.find( params[:user_id] )
+        redirect_to(root_url) unless @user == current_user
+    end
+
+    
+    
     
     private
     def profile_params 
